c7add97e011eb2702eb7c1598c20231b16654bfb
Max-212 <zte48228@gmail.com>
2020-04-07 15:27:08 +0300

    AudioPlayer

---
+++ AudioPlayer/Model/Audio.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AudioPlayer.Model
+{
+    public class Audio : BaseModel
+    {
+        private string nameView;
+        private string name;
+        private string genre;
+        private TimeSpan duration;
+        private int mark;
+        private string author;
+        private int year;
+
+
+        public string NameView { get => nameView; set { nameView = value; OnPropertyChanged(); } }
+        public string Name { get => name; set { name = value; OnPropertyChanged(); } }
+        public string Genre { get => genre; set { genre = value; OnPropertyChanged(); } }
+        public TimeSpan Duration { get => duration; set { duration = value; OnPropertyChanged(); } }
+        public int Mark { get => mark; set { mark = value; OnPropertyChanged(); } }
+        public string Author { get => author; set { author = value; OnPropertyChanged(); } }
+        public int Year { get => year; set { year = value; OnPropertyChanged(); } }
+
+        public Audio(string author, string name)
+        {
+            Author = author;
+            Name = name;
+            NameView = Author + " - " + Name;
+        }
+
+    }
+}
---
+++ AudioPlayer/ViewModel/MainViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using AudioPlayer.Model;
+using AudioPlayer.View;
+using Microsoft.VisualStudio.PlatformUI;
+
+namespace AudioPlayer.ViewModel
+{
+    public class MainViewModel : BaseModel
+    {
+        
+        private Audio selectedAudio;
+        private PlayList selectedPlayList = new PlayList();
+
+        
+        public ObservableCollection<PlayList> PlayLists { get; set; }
+        public PlayList SelectedPlayList { get => selectedPlayList; set { selectedPlayList = value; OnPropertyChanged(); } }
+        public Audio SelectedAudio { get => selectedAudio; set { selectedAudio = value; OnPropertyChanged(); } }
+
+        public ICommand AddPlayList
+        {
+            get
+            {
+                return new DelegateCommand( () =>
+                { 
+                    PlayLists.Add(new PlayList() { Name = AddPlayListWindow.Show("Enter playlist name") });
+                    SelectedPlayList = PlayLists.Last();
+
+                });
+            }
+        }
+
+        public ICommand DeletePlayList
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    if(!SelectedPlayList.Name.Equals("Default Play List"))
+                    {
+                        PlayLists.Remove(SelectedPlayList);
+                        SelectedPlayList = null;
+                        SelectedAudio = null;
+                    }
+                    
+                });
+            }
+        }
+
+        public ICommand DeleteAudio
+        {
+            get
+            {
+                return new DelegateCommand(() =>
+                {
+                    PlayLists[PlayLists.IndexOf(SelectedPlayList)].Audios.Remove(SelectedAudio);
+                    SelectedAudio = null;
+                });
+            }
+        }
+
+        public MainViewModel()
+        { 
+            PlayLists = new ObservableCollection<PlayList>();
+            
+            
+
+            PlayLists.Add(new PlayList() { Name = "Default Play List" });
+            PlayLists.Add(new PlayList() { Name = "List1"});
+            PlayLists.Add(new PlayList() { Name = "List2" });
+            PlayLists.Add(new PlayList() { Name = "List3" });
+            PlayLists.Add(new PlayList() { Name = "List4" });
+            PlayLists[0].Audios.Add(new Audio("Nirvana", "Smells like teen spirit") { Genre = "Grunge", Mark = 4});
+            PlayLists[0].Audios.Add(new Audio("Nirvana", "Rape me") { Genre = "Heavy Metal", Mark = 3 });
+            PlayLists[0].Audios.Add(new Audio("Nirvana", "Smells like teen spirit") { Genre = "Grunge", Mark = 4 });
+            PlayLists[0].Audios.Add(new Audio("Nirvana", "Rape me") { Genre = "Heavy Metal", Mark = 3 });
+            PlayLists[0].Audios.Add(new Audio("Nirvana", "Smells like teen spirit") { Genre = "Grunge", Mark = 4 });
+            PlayLists[0].Audios.Add(new Audio("Nirvana", "Rape me") { Genre = "Heavy Metal", Mark = 3 });
+            PlayLists[0].Audios.Add(new Audio("Nirvana", "Smells like teen spirit") { Genre = "Grunge", Mark = 4 });
+            PlayLists[0].Audios.Add(new Audio("Nirvana", "Rape me") { Genre = "Heavy Metal", Mark = 3 });
+
+            SelectedAudio = PlayLists[0].Audios[0];
+            PlayLists[1].Audios.Add(PlayLists[0].Audios[2]);
+            SelectedPlayList = PlayLists[0];
+            
+        }
+    }
+}
